5ec7fb04da618cf37bf6f81384d96285ea8bdebc
Ketan Gupta <ketan19972010@gmail.com>
2016-05-17 11:32:02 +0530

    Adds README and schema

---
+++ README.md
@@ -0,0 +1,10 @@
+# CodeRed | A Simple Platform for Holding Competitive Programming Contests
+
+This application is based on the MVC Framework and the backend is on PHP.
+The application uses [ToroPHP](https://github.com/anandkunal/ToroPHP) for routing and [Twig](https://github.com/twigphp/Twig) for templates.
+
+### Set Up : 
+- Clone the repository.
+- Copy over `config/config.example.php` to `config/config.php` and edit the values.
+- Import schema `schema/codered.sql`.
+- Install composer (`curl -sS https://getcomposer.org/installer | php`) and run `composer install` to install all the dependencies.
---
+++ schema/codered.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u1build0.15.04.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 17, 2016 at 11:27 AM
+-- Server version: 5.6.28-0ubuntu0.15.04.1
+-- PHP Version: 5.6.4-4ubuntu6.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `codered`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `problems`
+--
+
+CREATE TABLE IF NOT EXISTS `problems` (
+  `code` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `statement` text NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `accepted` int(5) NOT NULL,
+  `total` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `problems`
+--
+
+INSERT INTO `problems` (`code`, `name`, `statement`, `author`, `accepted`, `total`) VALUES
+('TEST', 'Life Universe and Everything', 'All submissions for this problem are available.\r\n\r\nFor help on this problem, please check out our tutorial Input and Output (I/O)\r\n\r\nYour program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.\r\nExample\r\n\r\n\r\nInput:\r\n1\r\n2\r\n88\r\n42\r\n99\r\n\r\nOutput:\r\n1\r\n2\r\n88', 'Ketan Gupta', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `name` varchar(20) NOT NULL,
+  `score` int(5) NOT NULL,
+  `penalty` int(10) NOT NULL,
+  `a` int(5) NOT NULL,
+  `b` int(5) NOT NULL,
+  `c` int(5) NOT NULL,
+  `d` int(5) NOT NULL,
+  `e` int(5) NOT NULL,
+  `f` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
